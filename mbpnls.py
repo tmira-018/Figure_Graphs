074647d126860c2de6ddf2b56f9a777ee571de6e
Tania Miramontes <tania.miramontes18@gmail.com>
2025-02-28 00:50:31 -0800

    fixed statistical testing to do full dataframe and editing plot titles suptitle and subplot titles

--- mbpnls.py
+++ mbpnls.py
@@ -1,3 +1,4 @@
+import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd 
@@ -5,61 +6,54 @@ import numpy as np
 from scipy import stats
 from scikit_posthocs import posthoc_dunn
 
+
 # Import data 
-nls_4dpf = pd.read_excel('/Users/miramota/OHSU Dropbox/Tania Miramontes/Data_sheets/WIN_mbp-nls_4dpf.xlsx',
+nls_4dpf = pd.read_excel('DataSheets/WIN_mbp-nls_4dpf.xlsx',
                          sheet_name= 'combined_spinal')
-nls_5dpf = pd.read_excel('/Users/miramota/OHSU Dropbox/Tania Miramontes/Data_sheets/WIN_mbp-nls_5dpf.xlsx',
+nls_5dpf = pd.read_excel('DataSheets/WIN_mbp-nls_4dpf.xlsx',
                          sheet_name= 'combined_spinal')
 
-def test_normality_wide(data, column):
+def test_normality_wide(data):
     """
-    Perform Shapiro-Wilk test and plot histogram on dataframe that is 
-    wide format.
+    Perform Shapiro-Wilk test and plot histogram on dataframe that is in a 
+    wide format. Will loop through all columns in the dataframe.
     
     Parameters:
     data: pandas DataFrame
     column: string, name of column to test
     """
-    data_analysis = data[column].dropna()
+    # Loop through all the  columns
+    for column in data.columns:
+        data_col = data[column].dropna()
 
-    # Perform Shapiro-Wilk test
-    statistic, p_value = stats.shapiro(data_analysis)
+        # Perform Shapiro-Wilk test
+        statistic, p_value = stats.shapiro(data_col)
+        
+        # Create figure with two subplots
+        fig, (ax1) = plt.subplots(1, figsize=(12, 5))
     
-    # Create figure with two subplots
-    fig, (ax1) = plt.subplots(1, figsize=(12, 5))
+        # Histogram with density plot
+        sns.histplot(data=data_col, kde=True,ax=ax1) 
+        ax1.set_title('Distribution of Abberant Count')
     
-    # Histogram with density plot
-    sns.histplot(data=data_analysis, kde=True,ax=ax1) 
-    ax1.set_title('Distribution of Abberant Count')
-    
-    plt.tight_layout()
-    
-    print(f"Shapiro-Wilk test results:")
-    print(f"Statistic: {statistic:.4f}")
-    print(f"P-value: {p_value:.4f}")
-    print(f"\nIf p-value > 0.05: Data is normally distributed")
-    print(f"If p-value < 0.05: Data is not normally distributed")
+        plt.tight_layout()
+        
+        print(f"Shapiro-Wilk test results for {column}:")
+        print(f"Statistic: {statistic:.4f}")
+        print(f"P-value: {p_value:.4f}")
+        print(f"If p-value > 0.05: Data is normally distributed")
+        print(f"If p-value < 0.05: Data is not normally distributed\n")
     
     return statistic, p_value, fig
 
 # Usage 4dpf
-stat, p_val, fig = test_normality_wide(nls_4dpf, 'DMSO')
-plt.show()
-stat, p_val, fig = test_normality_wide(nls_4dpf, '0.5 um')
-plt.show()
-stat, p_val, fig = test_normality_wide(nls_4dpf, '1.0 um')
-plt.show()
+test_normality_wide(nls_4dpf)
 
 # Usage 5dpf
-stat, p_val, fig = test_normality_wide(nls_5dpf, 'DMSO')
-plt.show()
-stat, p_val, fig = test_normality_wide(nls_5dpf, '0.5 um')
-plt.show()
-stat, p_val, fig = test_normality_wide(nls_5dpf, '1.0 um')
-plt.show()
+test_normality_wide(nls_5dpf)
 
-# make 5dpf dataframe long version
-nls5dpf_long = nls_5dpf.melt(var_name="Drug", value_name="Response")
+# Reformat dataframe - make 5dpf dataframe long version
+nls5dpf_long = nls_5dpf.melt(var_name="Drug", value_name="Number of Cells")
 nls5dpf_long = nls5dpf_long.dropna()
 
 # Perform Kruskal-Wallis test
@@ -73,8 +67,9 @@ H_stat5, p_val5 = stats.kruskal(nls_5dpf['DMSO'].dropna(),
                                 nls_5dpf['1.0 um'].dropna())
 print(f"Kruskal-Wallis Test: H = {H_stat5}, p = {p_val5}")
 
-# Perform Dunn's test post hoc
-nls_posthoc_5results = sp.posthoc_dunn(nls5dpf_long, val_col="Response", group_col="Drug", p_adjust="bonferroni")
+# Perform Dunn's test post hoc on 5 dpf data
+nls_posthoc_5results = sp.posthoc_dunn(nls5dpf_long, val_col="Number of Cells", 
+                                       group_col="Drug", p_adjust="bonferroni")
 print(nls_posthoc_5results)
 
 
@@ -86,17 +81,21 @@ sns.boxplot(data = nls_4dpf,
 sns.swarmplot(data = nls_4dpf,
               palette = ["#1768AC", "#F72585", "#420039"],
               legend = False, ax=ax1)
+ax1.spines['top'].set_visible(False)
+ax1.spines['right'].set_visible(False)
+ax1.set_title('4 dpf')
 sns.boxplot(data = nls_5dpf, 
             fill = False, widths = 0.25, linewidth= 0.75,
             palette = ['black', 'black', 'black'], ax=ax2)
 sns.swarmplot(data = nls_5dpf, 
               palette = ["#1768AC", "#F72585", "#420039"],
               legend = False, ax=ax2)
-ax1.spines['top'].set_visible(False)
-ax1.spines['right'].set_visible(False) 
+ 
 ax2.spines['top'].set_visible(False)
-ax2.spines['right'].set_visible(False) 
-plt.yticks(np.arange(0, 250, 50))  
+ax2.spines['right'].set_visible(False)  
 ax2.legend(loc = 'upper right')
-plt.savefig("/Users/miramota/Desktop/Figures/mbp_nls/mbpnls-45graph.pdf", format='pdf')
+ax2.set_title('5 dpf')
+plt.yticks(np.arange(0, 250, 50)) 
+fig.suptitle('Number of OL Nuclei')
+plt.savefig('/Figure_Outputs/mbpnls-45graph.pdf', format='pdf')
 plt.show()
