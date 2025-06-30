b1038c4fd7d3be343934f73094553d2e4b87aef7
Tania Miramontes <tania.miramontes18@gmail.com>
2025-06-30 16:48:44 -0700

    added graph for cell count and statistics

--- vo103.py
+++ vo103.py
@@ -18,28 +18,52 @@ mutant_df = pd.read_excel("DataSheets/vo103_mbpnlscaax_24.xlsx",
 # Edit the dataframe to include only 5dpf data
 mutant_5dpf = mutant_df[mutant_df['dpf']== 5]
 
+
 # Exclude the WIN05μM condition
 mutant_5dpf_01 = mutant_5dpf[mutant_5dpf['condition'] != 'WIN05μM']
+mutant_5dpf_01 = mutant_5dpf_01.dropna()
 # Separate data by  
 
+# Plotting aberrant data
 f, ax = plt.subplots()
 sns.swarmplot(data = mutant_5dpf_01, x = 'genotype', y = 'aberrant',
               hue= 'condition',
               dodge = True,
               order=['wt', 'het', 'mut'],
               #hue_order=['wt', 'het', 'mut'],
-              palette = ["#095256","#6DD6DA", "#6588D4"],
+              palette = ["#1768AC", "#F72585"],
               legend= True)
 
 sns.boxplot(data = mutant_5dpf_01, x = 'genotype',
            y = 'aberrant', hue = 'condition', 
            order=['wt', 'het', 'mut'],
            fill = False, widths = 0.18,
-           palette= ['black', 'black', 'black'],
+           palette= ['black', 'black'],
            legend=False, linewidth=0.75)
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 plt.savefig('Figure_Outputs/vo103_aberrant_genotyped.pdf', format='pdf')
+plt.show()
+
+# Plotting nls counts 
+
+f, ax = plt.subplots()
+sns.swarmplot(data = mutant_5dpf_01,  x = 'genotype', y = 'nls',
+              hue = 'condition',
+              dodge = True,
+              order = ['wt', 'het', 'mut'],
+              palette = ["#1768AC", "#F72585"],
+              legend= True)
+sns.boxplot(data = mutant_5dpf_01, x = 'genotype', y = 'nls',
+            hue= 'condition',
+            order = ['wt', 'het', 'mut'],
+            fill = False, widths = 0.18,
+            palette= ['black', 'black'],
+            legend = False, linewidth=0.75)
+ax.set_ylim(0,50)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+plt.savefig('Figure_Outputs/vo103_nlscounts.pdf', format = "pdf")
 plt.show()
 
 def test_normality(data, condition, column='aberrant'):
@@ -97,3 +121,64 @@ print(result)
 tukey = pairwise_tukeyhsd(mutant_5dpf_02['aberrant'], 
                           mutant_5dpf_02['genotype'] + mutant_5dpf_02['condition'])
 print(tukey)
+
+# nls
+mutant_5dpf_nls = mutant_5dpf_01.dropna(subset=['nls'])
+# Do two-way ANOVA
+nls_model = ols( 
+    'nls ~ C(genotype) + C(condition) + C(genotype):C(condition)', 
+    data=mutant_5dpf_nls).fit() 
+result = sm.stats.anova_lm(nls_model, typ=2) 
+print(result)
+
+nls_tukey = pairwise_tukeyhsd(mutant_5dpf_nls['nls'], 
+                          mutant_5dpf_nls['genotype'] + mutant_5dpf_nls['condition'])
+print(nls_tukey)
+
+nls_tukey = sp.posthoc_tukey(mutant_5dpf_nls, val_col= 'nls',
+                             group_col= 'condition')
+print(nls_tukey)
+
+
+def cond_gen_types(data, condition, cond_column, genotype, gen_column):
+    """
+    Seperate data by condition.
+
+    Returns: 
+    data_condition (pandas DataFrame),
+
+    Parameters:
+    data: pandas DataFrame
+    condition: string or integer of the condition
+    column: string, name of the colum to be seperated
+    """
+    data_condition = data[data[cond_column] == condition]
+    gen_df = data_condition[data_condition[gen_column] == genotype]
+
+    return gen_df
+
+
+dmso_wt = cond_gen_types(mutant_5dpf_nls, 'DMSO', 'condition', 'wt', 'genotype')
+dmso_het = cond_gen_types(mutant_5dpf_nls, 'DMSO', 'condition', 'het', 'genotype')
+dmso_mut = cond_gen_types(mutant_5dpf_nls, 'DMSO', 'condition', 'mut', 'genotype')
+
+wt = cond_gen_types(mutant_5dpf_nls, 'wt', 'genotype')
+
+wt_nls_tukey = sp.posthoc_tukey(mutant_5dpf_nls, val_col = 'nls', group_col = 'genotype')
+print(wt_nls_tukey)
+
+#run t-test
+dmso_wt_nls = dmso_wt['nls'].to_numpy()
+win_wt_nls = win_wt['nls'].to_numpy()
+t_stat, p_value = stats.ttest_ind(dmso_wt_nls, win_wt_nls)
+print(f"t-statistic: {t_stat}, p-value: {p_value}")
+
+dmso_mut_nls  = dmso_mut['nls'].to_numpy()
+win_mut_nls = win_mut['nls'].to_numpy()
+t_stat, p_value = stats.ttest_ind(dmso_mut_nls, win_mut_nls)
+print(f"t-statistic: {t_stat}, p-value: {p_value}")
+
+win_wt = cond_gen_types(mutant_5dpf_nls, 'WIN1μM', 'condition', 'wt', 'genotype')
+win_het = cond_gen_types(mutant_5dpf_nls, 'WIN1μM', 'condition', 'het', 'genotype')
+win_mut = cond_gen_types(mutant_5dpf_nls, 'WIN1μM', 'condition', 'mut', 'genotype')
+
