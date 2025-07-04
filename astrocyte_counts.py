d87381cf81eb6177d8def27b0db11e665f6a430b
Tania Miramontes <tania.miramontes18@gmail.com>
2025-07-04 12:27:02 -0700

    added statistical testing

--- astrocyte_counts.py
+++ astrocyte_counts.py
@@ -26,6 +26,10 @@ sns.stripplot(data = astro_4dpf,
 ax1.spines['top'].set_visible(False)
 ax1.spines['right'].set_visible(False)
 ax1.set_ylim(0, 250)
+ax1.scatter(0, mean_5dpf_dmso, color = 'black', marker = 'o',
+           s = 75)
+ax1.scatter(1, mean_5dpf_1um, color = 'black', marker = 'o',
+            s = 75)
 ax1.set_title('4 dpf')
 sns.boxplot(data= astro_5dpf,
             x = 'condition', y= 'cell_count',
@@ -47,12 +51,16 @@ plt.savefig('Figure_Outputs/astrocyte_counts.pdf', format = 'pdf')
 # make numpy array for stats
 astro_4dpf_dmso = astro_4dpf[astro_4dpf['condition'] == 0]['cell_count'].to_numpy()
 astro_4dpf_1um = astro_4dpf[astro_4dpf['condition'] == 1]['cell_count'].to_numpy()
+mean_4dpf_dmso= np.mean(astro_4dpf_dmso)
+mean_4dpf_1um = np.mean(astro_4dpf_1um)
 
-tstat_4dpf, pvalue_4dpf = stats.ttest_ind(astro_4dpf_dmso, astro_4dpf_1um)
+tstat_4dpf, pvalue_4dpf = stats.ttest_ind(astro_4dpf_dmso, astro_4dpf_1um, equal_var= True)
 print(f't-statistic: {tstat_4dpf}, p-value: {pvalue_4dpf}')
 
 astro_5dpf_dmso = astro_5dpf[astro_5dpf['condition'] == 0]['cell_count'].to_numpy()
 astro_5dpf_1um = astro_5dpf[astro_5dpf['condition'] == 1]['cell_count'].to_numpy()
+mean_5dpf_dmso = np.mean(astro_5dpf_dmso)
+mean_5dpf_1um = np.mean(astro_5dpf_1um)
 
-tstat_5dpf, pvalue_5dpf = stats.ttest_ind(astro_5dpf_dmso, astro_5dpf_1um)
+tstat_5dpf, pvalue_5dpf = stats.ttest_ind(astro_5dpf_dmso, astro_5dpf_1um, equal_var= True)
 print(f't-statistic: {tstat_5dpf}, p-value: {pvalue_5dpf}')
